4018b83c91c2e862ba68319e1bba311eb875426e
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-12-09 18:19:26 -0500

    Abstracts graph logic to Graph class in ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,14 +1,30 @@
 from collections import deque
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if all(v in self.vertices for v in [v1, v2]):
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("At least one of the given vertices does not exist.")
+    def get_neighbors(self, vertex_id):
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            raise IndexError("That vertex does not exist.")
+
+
 def earliest_ancestor(ancestors, starting_node):
     # Create a family tree of child keys w/ list of parent values
-    fam_tree = {}
+    fam_tree = Graph()
     for (parent, child) in ancestors:
-        # If the child's already there, add the parent
-        if child in fam_tree:
-            fam_tree[child].append(parent)
-        # Otherwise, create the child with the parent
-        else:
-            fam_tree[child] = [parent]
+        fam_tree.add_vertex(child)
+        fam_tree.add_vertex(parent)
+        fam_tree.add_edge(child, parent)
     # Initialize tracking variables
     ancestor = starting_node
     generations = 0
@@ -25,8 +41,8 @@ def earliest_ancestor(ancestors, starting_node):
         elif child_gens == generations and child < ancestor:
             ancestor = child
         # If the child has known parents...
-        if child in fam_tree:
+        if len(parents := fam_tree.get_neighbors(child)) > 0:
             # Extend the stack with their parents and the generations increased by 1
-            stack.extend([(parent, child_gens + 1) for parent in fam_tree[child]])
-    # Return -1 if 
+            stack.extend([(parent, child_gens + 1) for parent in parents])
+    # Return -1 if starting_node had no parents
     return -1 if generations == 0 else ancestor
